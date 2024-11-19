109c98d856f537ad2ecfcfc6b34c1df893ad8f01
jvm3 <jamesandrewpike@gmail.com>
2024-11-19 09:15:28 -0500

    commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,25 @@
+import { component$} from "@builder.io/qwik";
+// import type { DocumentHead } from "@builder.io/qwik-city";
+
+// import { SITE } from "../config.mjs";
+
+
+ 
+export default component$(() => {
+
+  return (
+    <>
+
+    </>
+  );
+});
+
+// export const head: DocumentHead = {
+//   title: SITE.title,
+//   meta: [
+//     {
+//       name: "Etchy Sketchy",
+//       content: SITE.description,
+//     },
+//   ],
+// };
